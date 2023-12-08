29bfacf2eedcb94a1ed3ec07514c058b66591f2b
Onischenko Kostyantyn <Onischenko411@gmail.com>
2023-12-08 23:10:18 +0200

    change

--- js.js
+++ js.js
@@ -1,127 +1,11 @@
-// debugger
-// let input = prompt ('Enter number')
-// while(!input?.trim() || isNaN(input)){
-//     input = prompt ('Invalid number.Enter your number')
-//     if(input === null){
-//         break;
-//     }
-// }
-// if(input === null ){
-//     alert('Okay..')
-// }
-// else{
-// const num = +input
-// alert (num +'^ 2= ' + num **2)}
-// debugger
-// // const numbers = [5, 8, -1, '-6', 11, 'ololo', '-2']
-// // let sum = 0
-// // for ( let i = 0; i < numbers.length; i++ ){
-// //     sum += numbers [i]
-// // }
-// // alert('Sum is ' + sum)
-// const a = 'abc' && 10 || null && true
-// console.log(a)
-// if ( (" " && Number)|| (null && true)){
-//     alert("here")
-//     const w = prompt('SFDSF')
-// }
-// else if (w){
-//     alert('Не вийшло')
-// }
-
-// let i = 0
-// while( i <= 5) {
-//     // alert(i)
-//     console.log(i)
-//     i++
-    
-// }
-// let x = prompt('Enter your number')
-// while(!x?.trim() || isNaN(x)){
-//     x = prompt ('Invalid number.Enter your number')
-//     if(x===null){
-//         break;
-//     }
-// }
-// if(x === null){
-//     alert('Okay....')
-// }
-// else{
-//     const num = +x
-// alert(num + '^ 2 =' + num ** 2)
-// }
-// let num = 123456
-
-// let result = ''
-// while(num > 0){
-//     result = ' ' + (num % 10) + result
-//     num = (num - num % 10) / 10
-    
-// }
-// alert ('result :' + result)
-// const j =  -912312
-// let absNum = Math.abs(num)
-// let result =" "
-// while (absNum > 0){
-//     result = " " + (absNum % 10)
-// }
-// debugger
-// const num = -12312
-// console.log(num)
-// let absNum = Math.abs(num)
-// console.log(absNum)
-// let result = ""
-// console.log(result)
-// while(absNum > 0){
-//     const lastDigit = absNum % 10
-//     result = ' ' +lastDigit + result //// 
-//     console.log(result)
-//     absNum = ( absNum - absNum % 10)/10
-//     console.log(absNum)
-// }
-// alert('Result:' + ( num < 0 ? '-' : '') + result.trim())
-// console.log('Result:' + ( num < 0 ? '-' : '') + result.trim())
 debugger
-// const numbers = [5,8,-1,'-6',11,'ololo','-2'] 
-// const userNumber = isNaN(numbers)
-// let sum = 0
-// for(let i = 0; i< numbers.length; i++){
-//     const ValueNumber = parseFloat(numbers[i])
-//     sum += isNaN(ValueNumber)?0 : ValueNumber;
-   
-// }
-// alert('Sum is ' + sum)
-
 let userHryvnya = 40 ;
 
 for(let dollars = 10; dollars <= 100;dollars+=10) {
     let Valuta = dollars * userHryvnya;
     alert(`${dollars} долларів = ${Valuta} гривень`)}
 
-// const userEnterValidNum = +prompt('Enter number')
 
-// if (userEnterValidNum === null ){
-//     alert('As you wish')
-// }
-// else if (!userEnterValidNum.trim() || isNaN(userEnterValidNum)){
-//     alert('Error: invalid number!')
-// }
-// else{
-//     const numberPrime = +userEnterValidNum
-//     if(!Number.isInteger(numberPrime) || numberPrime < 0){
-//         alert(`This ${numberPrime} is not prime number`)
-//     }
-//     else{
-        
-        
-//         for(let i =2; i <= n;i++ ){
-//             for(let j = 2; j < i; j++){
-//                 if(i%j === 0)continue 
-//             }
-//             alert(i);
-//         }
-//     }
-// }
 const userEnterValidNum = prompt('Введіть число');
 
 if (userEnterValidNum === null) {
@@ -149,12 +33,30 @@ if (userEnterValidNum === null) {
     }
   }
 }
-let UserAddNumber = prompt('Введіть число')
 
-if(UserAddNumber % 3 === 0 && UserAddNumber){
-    alert('Число можно отримати')
-}
-else {
-    alert('Число не можно отримати')
-}
+let UserAddNumber = prompt('Введіть число');
 
+if (UserAddNumber === null || isNaN(UserAddNumber)) {
+    alert('Введіть число!');
+  } else {
+    // Перетворення введеного рядка в число
+    UserAddNumber = +UserAddNumber;
+  
+    // Флаг для визначення, чи число можна отримати
+    let isNumberObtainable = false;
+  
+    // Перевірка, чи число можна отримати для значень від 1 до введеного числа
+    for (let i = 1; i <= UserAddNumber; i++) {
+      if (UserAddNumber % i === 0 && i % 3 === 0) {
+        isNumberObtainable = true;
+        break; 
+      }
+    }
+  
+    
+    if (isNumberObtainable) {
+      alert('Число можна отримати');
+    } else {
+      alert('Число не можна отримати');
+    }
+  }
