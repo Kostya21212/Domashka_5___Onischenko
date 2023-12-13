6490756d95f9df61e19de9c86a9358ee47aa1586
Onischenko Kostyantyn <Onischenko411@gmail.com>
2023-12-13 22:50:15 +0200

    Changes

--- js.js
+++ js.js
@@ -1,70 +1,72 @@
-debugger
-const userHryvnya = 40 ;
+// debugger
+// const userHryvnya = 40 ;
 
-for(let dollars = 10; dollars <= 100;dollars+=10) {
-    let valuta = dollars * userHryvnya;
-    alert(`${dollars} долларів = ${valuta} гривень`)}
+// for(let dollars = 10; dollars <= 100;dollars+=10) {
+//     const valuta = dollars * userHryvnya;
+//     alert(`${dollars} долларів = ${valuta} гривень`)}
 
 
-const userEnterValidNum = prompt('Введіть число');
+// const userEnterValidNum = prompt('Введіть число');
 
-if (userEnterValidNum === null) {
-  alert('Як ви бажаєте');
-} else {
-  const input = userEnterValidNum.trim(); 
-  const parsedNum = +input;
+// if (userEnterValidNum === null) {
+//   alert('Як ви бажаєте');
+// } else {
+//   const input = userEnterValidNum.trim(); 
+//   const parsedNum = +input;
 
-  let isPrime = true;
+//   let isPrime = true;
 
-  if (!input || !Number.isInteger(parsedNum) || parsedNum < 2) {
-    isPrime = false;
-  } else {
-    const numberPrime = parsedNum;
+//   if (!input || !Number.isInteger(parsedNum) || parsedNum < 2) {
+//     isPrime = false;
+//   } else {
+//     const numberPrime = parsedNum;
 
-    for (let i = 2; i <= Math.sqrt(numberPrime); i++) {
-      if (numberPrime % i === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-  }
+//     for (let i = 2; i <= Math.sqrt(numberPrime); i++) {
+//       if (numberPrime % i === 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//   }
 
-  if (!isPrime) {
-    alert(` Не є простим числом `);
-  } else {
-    alert(`Є простим числом`);
-  }
-}
-
+//   if (!isPrime) {
+//     alert(` Не є простим числом `);
+//   } else {
+//     alert(`Є простим числом`);
+//   }
+// }
 
 let UserAddNumber = prompt('Введіть число');
-
 
 if (UserAddNumber === null) {
   alert('Як ви бажаєте');
 } else {
-  
   const trimmedInput = UserAddNumber.trim();
+  const numericValue = Number(trimmedInput);
 
-  if (trimmedInput === '' || isNaN(trimmedInput)) {
+  if (trimmedInput === '' || isNaN(numericValue)) {
     alert('Помилка: введено некоректне число або порожній рядок');
   } else {
-   
     UserAddNumber = +trimmedInput;
 
-    
-    let isNumberObtainable = false;
+    let isPowerOfThree = true;
+    let userNumber = UserAddNumber;
 
-    
-    for (let i = 1; i <= UserAddNumber; i++) {
-      if (UserAddNumber % i === 0 && (i % 3 === 0 )) {
-        isNumberObtainable = true;
+    while (userNumber > 1) {
+      if (userNumber % 3 !== 0 || userNumber === 1) {
+        isPowerOfThree = false;
         break;
       }
+      userNumber /= 3;
     }
+
     alert(
-      trimmedInput === '' || isNaN(trimmedInput) || /^0\d+/.test(trimmedInput) || trimmedInput <= 0
+      trimmedInput === '' || trimmedInput <= 0
         ? 'Число не можна отримати або введено некоректне число'
-        : `Число ${+trimmedInput % 3 === 0 ? 'можна отримати' : 'не можна отримати'}`
+        : `Число ${isPowerOfThree ? 'можна отримати' : 'не можна отримати'} `
     );
-}}
+  }
+}
+
+
+
